815677b467b19f4c9b4fd8e195bd79fa9aafc183
Daniel Zaoui <daniel.zaoui@samsung.com>
2015-10-28 16:43:51 +0200

    Fix includes usage

--- src/lib/eina/eina_thread.c
+++ src/lib/eina/eina_thread.c
@@ -34,6 +34,7 @@
 
 # include <pthread.h>
 # include <errno.h>
+# include <string.h>
 
 #ifdef EINA_HAVE_PTHREAD_AFFINITY
 #ifndef __linux__
@@ -110,22 +111,16 @@ _eina_internal_call(void *context)
 {
    Eina_Thread_Call *c = context;
    void *r;
-#ifdef EINA_HAVE_DEBUG
    pthread_t self;
-#endif
 
    if (c->prio == EINA_THREAD_BACKGROUND ||
        c->prio == EINA_THREAD_IDLE)
      eina_sched_prio_drop();
 
-#ifdef EINA_HAVE_DEBUG
    self = pthread_self();
    _eina_debug_thread_add(&self);
-#endif
    r = c->func((void*) c->data, eina_thread_self());
-#ifdef EINA_HAVE_DEBUG
    _eina_debug_thread_del(&self);
-#endif
 
    free(c);
 
