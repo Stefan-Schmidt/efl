439186d9bf3e20cdfc208e5739bb58bfafbac0a3
Daniel Hirt <daniel.hirt@samsung.com>
2016-04-25 09:52:10 +0300

    Evas textblock: port cursor_word_start to eo

--- src/lib/evas/canvas/evas_object_textblock.c
+++ src/lib/evas/canvas/evas_object_textblock.c
@@ -8262,12 +8262,19 @@ evas_textblock_cursor_format_prev(Evas_Textblock_Cursor *cur)
 EAPI Eina_Bool
 evas_textblock_cursor_word_start(Evas_Textblock_Cursor *cur)
 {
+   return evas_obj_textblock_cursor_word_start(cur->obj, cur);
+}
+
+EOLIAN static Eina_Bool
+_evas_textblock_cursor_word_start(Eo *eo_obj,
+      Evas_Textblock_Data *o EINA_UNUSED, Evas_Textblock_Cursor *cur)
+{
    const Eina_Unicode *text;
    size_t i;
    char *breaks;
 
    if (!cur) return EINA_FALSE;
-   Evas_Object_Protected_Data *obj = eo_data_scope_get(cur->obj, EVAS_OBJECT_CLASS);
+   Evas_Object_Protected_Data *obj = eo_data_scope_get(eo_obj, EVAS_OBJECT_CLASS);
    evas_object_async_block(obj);
    TB_NULL_CHECK(cur->node, EINA_FALSE);
 
