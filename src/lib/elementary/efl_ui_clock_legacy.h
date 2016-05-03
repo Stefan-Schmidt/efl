9df9f59522da79312505395b2260ffd6801af653
Yeshwanth Reddivari <r.yeshwanth@samsung.com>
2016-05-03 12:49:34 +0530

    add Efl.Ui.Clock skeleton

---
+++ src/lib/elementary/efl_ui_clock_legacy.h
@@ -0,0 +1,15 @@
+/**
+ * @brief Adds a new clock Widget
+ *
+ * The default clock format and corresponding strings are based on current locale.
+ *
+ * @param parent The parent object
+ * @return The new object or NULL if it cannot be created
+ *
+ * This function inserts a new clock widget on the canvas.
+ *
+ * @ingroup Elm_clock
+ */
+EAPI Evas_Object *efl_ui_clock_add(Evas_Object *parent);
+
+#include "efl_ui_clock.eo.legacy.h"
