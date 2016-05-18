0072012e18ccd89499e0efcb429de11b962cacd3
Jean-Philippe ANDRE <jpeg@videolan.org>
2016-05-19 03:20:09 +0900

    Efl: Add Efl.Part and switch to it
    This should now fix the part API usage once and for all.
    EFL should have no part name in any of its APIs beyond
    the Efl.Part interface.
    
    Part proxy objects (may be real objects) have a lifetime
    of only one function call, in a fashion similar to eo_super.
    
    @feature

--- src/lib/edje/edje_util.c
+++ src/lib/edje/edje_util.c
@@ -3897,23 +3897,15 @@ _edje_object_efl_container_content_get(Eo *obj, Edje *ed, const char *part)
    rp = _edje_real_part_recursive_get(&ed, part);
    if (!rp) return NULL;
 
-   switch (rp->type)
+   if (rp->type != EDJE_RP_TYPE_SWALLOW)
      {
-      case EDJE_RP_TYPE_SWALLOW:
-       if (!rp->typedata.swallow) return NULL;
-       return rp->typedata.swallow->swallowed_object;
-      case EDJE_RP_TYPE_CONTAINER:
-        if (rp->part->type == EDJE_PART_TYPE_BOX)
-          return _edje_box_internal_proxy_get(obj, ed, rp);
-        else if (rp->part->type == EDJE_PART_TYPE_TABLE)
-          return _edje_table_internal_proxy_get(obj, ed, rp);
-        else return NULL;
-      case EDJE_RP_TYPE_TEXT:
-        WRN("not implemented yet");
-        return NULL;
-      default:
+        ERR("Edje group '%s' part '%s' is not a swallow. Did "
+            "you mean to call efl_part() instead?", ed->group, part);
         return NULL;
      }
+
+   if (!rp->typedata.swallow) return NULL;
+   return rp->typedata.swallow->swallowed_object;
 }
 
 /* new in eo */
