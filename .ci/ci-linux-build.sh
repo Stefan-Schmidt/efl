da4bd8f8ac1a96f9a3d0d4025ecb07179bb358ca
Stefan Schmidt <stefan@osg.samsung.com>
2017-06-08 01:40:14 +0200

    ci: factor out testsuite run into separate script

--- .ci/ci-linux-build.sh
+++ .ci/ci-linux-build.sh
@@ -8,11 +8,3 @@ PARALLEL_JOBS=10
 make -j $PARALLEL_JOBS
 make -j $PARALLEL_JOBS examples
 make -j $PARALLEL_JOBS benchmark
-
-# Prepare test setup
-#export DISPLAY=:99.0
-#sh -e /etc/init.d/xvfb start
-#sleep 3
-#eval $(dbus-launch --sh-syntax --exit-with-session)
-#make -j 10 check
-#cat src/tests/ecore/*log
