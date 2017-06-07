4903c93b7de0c1935d0df7a16d2a4aac38d89e5d
Stefan Schmidt <stefan@osg.samsung.com>
2017-06-08 01:13:23 +0200

    ci: factor out dist build into a separate script

--- .ci/ci-linux-build.sh
+++ .ci/ci-linux-build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#COPTS="--with-tests=regular --disable-cxx-bindings"
 COPTS="--with-tests=regular"
 PARALLEL_JOBS=10
 
@@ -10,9 +9,6 @@ make -j $PARALLEL_JOBS
 make -j $PARALLEL_JOBS examples
 make -j $PARALLEL_JOBS benchmark
 
-#make -j 10 dist && mkdir tmp && cp efl*.gz tmp/ && cd tmp/ && tar xf efl* && cd efl*
-#./configure $COPTS && make -j 10 && make -j 10 examples && make -j 10 benchmark && make -j 10 check && cd ../..
-
 # Prepare test setup
 #export DISPLAY=:99.0
 #sh -e /etc/init.d/xvfb start
@@ -20,5 +16,3 @@ make -j $PARALLEL_JOBS benchmark
 #eval $(dbus-launch --sh-syntax --exit-with-session)
 #make -j 10 check
 #cat src/tests/ecore/*log
-
-#make -j 10 distcheck
